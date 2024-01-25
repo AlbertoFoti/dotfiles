998216a45a592f02b78d94708971ceba40cd7dcf
albertofots <albertofots@gmail.com>
2024-01-25 12:27:52 +0100

    back to rofi

--- install.sh
+++ install.sh
@@ -223,7 +223,7 @@ copy_configs(){
     cp -r ./wallpapers "$HOME"/Pictures/wallpapers
     echo -e "${green}[*] Copying zsh configs.${no_color}"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    sudo cp "$HOME"/dotfiles/keyitdev.zsh-theme "$HOME"/.oh-my-zsh/custom/themes
+    sudo cp "$HOME"/dotfiles/config/oh-my-zsh/keyitdev.zsh-theme "$HOME"/.oh-my-zsh/custom/themes
     cp "$HOME"/dotfiles/.zshrc "$HOME"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
